e3bca66711a78d6cbaf7f6d1c78fc55ca740cd3a
prityr <prityr@moneymul.com>
2024-07-25 16:23:04 +0530

    models-added

--- cms-app/pkg/models/blog.go
+++ cms-app/pkg/models/blog.go
@@ -1,5 +1,15 @@
 package models
 
+import "gorm.io/gorm"
+
 type Blog struct {
-	
+	gorm.Model
+	ID          uint   `gorm:"primaryKey"`
+	Title       string `gorm:"not null"`
+	Description string
+	CategoryID  uint
+	AuthorID    uint
+	Category    Category `gorm:"foreignKey:CategoryID"`
+	Author      Author   `gorm:"foreignKey:AuthorID"`
+	Image       string
 }
--- cms-app/pkg/models/category.go
+++ cms-app/pkg/models/category.go
@@ -1,1 +1,10 @@
 package models
+
+import "gorm.io/gorm"
+
+type Category struct {
+	gorm.Model
+	ID    uint   `gorm:"primaryKey"`
+	Name  string `gorm:"unique;not null"`
+	Image string
+}
